4ba011f7f487fc363f6ef11bb50f0899437d9483
Jaswanth Bommidi <jassu.bommidi@gmail.com>
2020-08-20 22:03:55 +0530

    Added customizable Path to VMs and masterlist function

--- src/server/VM.py
+++ src/server/VM.py
@@ -1,6 +1,6 @@
 from binascii import hexlify, unhexlify
 from shutil import rmtree 
-from os import mkdir, chdir, path, getcwd, remove, rmdir, listdir
+from os import makedirs, path, remove, listdir, walk
 from dataclasses import dataclass
 
 @dataclass
@@ -13,37 +13,39 @@ class VMStruct:
     ram: int = 4
 
 class VM:
+    def __init__(self):
+        self.region = "/tmp/VMs"
+        return
     def create(self, funds, account, name, tag):
         if(not (funds > 100)):
             return 2
-        if(not path.exists(path.join(getcwd(), account))):
-            mkdir(account)
+        if(not path.exists(path.join(self.region, account))):
+            makedirs(path.join(self.region, account))
         vm = VMStruct(name=name, tag=tag, tcpPorts=[22,80], udpPorts=[53])
-        content = hexlify(str(vm).encode())
-        if(not path.exists(path.join(getcwd(), account, tag))):
-            open(path.join(getcwd(), account, tag), 'w').write(content.decode())
+        if(not path.exists(path.join(self.region, account, tag))):
+            open(path.join(self.region, account, tag), 'w').write(hexlify(str(vm).encode()).decode())
         else:
             return 1
         return 0
     
     def modifyFirewall(self, account, tag, proto, port, operation):
-        if(not path.exists(path.join(getcwd(), account, tag))):
+        if(not path.exists(path.join(self.region, account, tag))):
             return 2
         else:
             pass
         
-        vm = eval(unhexlify(open(path.join(getcwd(), account, tag)).read()))
+        vm = eval(unhexlify(open(path.join(self.region, account, tag)).read()))
         if(operation == 0):
             if(not (port in eval("vm.{}Ports".format(proto)))):
                 eval("vm.{}Ports".format(proto)).append(port)
-                open(path.join(getcwd(), account, tag), 'w').write(hexlify(str(vm).encode()).decode())
+                open(path.join(self.region, account, tag), 'w').write(hexlify(str(vm).encode()).decode())
                 return 0
             else:
                 return 1
         else:
             if(port in eval("vm.{}Ports".format(proto))):
                 eval("vm.{}Ports".format(proto)).remove(port)
-                open(path.join(getcwd(), account, tag), 'w').write(hexlify(str(vm).encode()).decode())
+                open(path.join(self.region, account, tag), 'w').write(hexlify(str(vm).encode()).decode())
                 return 0
             else:
                 return 1
@@ -61,12 +63,12 @@ class VM:
                 'cpu': 70,
         }
 
-        if(not path.exists(path.join(getcwd(), account, tag))):
+        if(not path.exists(path.join(self.region, account, tag))):
             return 2
         else:
             pass
 
-        vm = eval(unhexlify(open(path.join(getcwd(), account, tag)).read()))
+        vm = eval(unhexlify(open(path.join(self.region, account, tag)).read()))
         cost = prices[resource] * count
         if(operation == 0):
             if(cost > balance):
@@ -75,7 +77,7 @@ class VM:
                 pass
 
             exec("vm.{} += count".format(resource))
-            open(path.join(getcwd(), account, tag), 'w').write(hexlify(str(vm).encode()).decode())
+            open(path.join(self.region, account, tag), 'w').write(hexlify(str(vm).encode()).decode())
             return 0
 
         else:
@@ -84,14 +86,14 @@ class VM:
             else:
                 pass
             exec("vm.{} -= count".format(resource))
-            open(path.join(getcwd(), account, tag), 'w').write(hexlify(str(vm).encode()).decode())
+            open(path.join(self.region, account, tag), 'w').write(hexlify(str(vm).encode()).decode())
             return 0
 
     def deleteVM(self, account, tag):
-        if(not path.exists(path.join(getcwd(), account, tag))):
+        if(not path.exists(path.join(self.region, account, tag))):
             return 1
         else:
-            remove(path.join(path.join(getcwd(), account, tag)))
+            remove(path.join(path.join(self.region, account, tag)))
             return 0
 
     def removeAccount(self, account, challenge):
@@ -99,20 +101,20 @@ class VM:
             return 2 # incorrect password
         else:
             pass
-        if(path.exists(path.join(getcwd(), account))):
-            rmtree(account)
+        if(path.exists(path.join(self.region, account))):
+            rmtree(path.join(self.region, account))
             return 0 # successfully deleted the account
         else:
             return 1 # user not found
 
     def statusofVM(self, account, tag): #the vulnerable function
-        if(not path.exists(path.join(getcwd(), account, tag))):
+        if(not path.exists(path.join(self.region, account, tag))):
             data = "The VM with the tag {} does not exist".format(tag)
             return data
         else:
             pass
 
-        vm = eval(unhexlify(open(path.join(getcwd(), account, tag)).read()))
+        vm = eval(unhexlify(open(path.join(self.region, account, tag)).read()))
         data = """
         VM Name: {}
         VM Tag: {}
@@ -123,10 +125,17 @@ class VM:
         return data
 
     def listmyVMs(self, account):
-        if(not path.exists(path.join(getcwd(), account))):
+        if(not path.exists(path.join(self.region, account))):
             return -1
         else:
             pass
         
-        vmtags = '\n'.join(listdir(path.join(getcwd(), account)))
+        vmtags = '\n'.join(listdir(path.join(self.region, account)))
         return vmtags
+
+    def masterlist(self):
+        vms = []
+        for r, d, f in walk(path.join(self.region)):
+            for vm in f:
+               vms.append(vm) 
+        return '\n'.join(vms)
