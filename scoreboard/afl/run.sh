fde666688df067c305d54e70d16e136bbced2eee
Jaswanth Bommidi <jassu.bommidi@gmail.com>
2020-08-30 00:50:57 +0530

    Completed Scoreboard

---
+++ scoreboard/afl/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 /home/f4lc0n/CTFs/blackStab/CTF/blackStab/scoreboard/afl/score.py
--- scoreboard/afl/score.py
+++ scoreboard/afl/score.py
@@ -1,84 +1,93 @@
+from os import path
 from datetime import datetime
 from time import time, sleep
 from requests import get
 
-START = 1598965200
-END = 1598718650
+START = 1598706000
+END = 1598734800
 def template():
     scrapi = get("http://localhost:8080/api/scoreboard").text
     roundapi = get("http://localhost:8080/api/round").text
     teamsapi = get("http://localhost:8080/api/teams").text
-    roundapi = get("http://localhost/api/round").text
+    roundapi = get("http://localhost:8080/api/round").text
     x="""
-                              bi0s internal 28-08-2020 (blackStab)\n"""
+                             \x1b[1;93mbi0s internal \x1b[1;96m28-08-2020 \x1b[1;92m(blackStab)\n"""
 
-    x+="""\n┌─ service milestones ────────────────────────────────────┬─ attack stats ──────┐"""
-    x+="""\n│     time in play : 0 days, 0 hrs, 0 min, 12 sec         │  rounds done : 27   │""".format(timediffer(int(time())-START), roundsdone(scrapi, 1))
-    x+="""\n│  from firstblood : 0 days, 0 hrs, 0 min, 11 sec         │flags submitted : 4  │""".format(timefirstblood(), fsubmitted(scrapi))
-    x+="""\n│  firstblood team : bi0s                                 │ teams pwning : 1    │""".format(fbloodteam(scrapi), teamspwning(scrapi))
-    x+="""\n│   time remaining : none seen yet                        │   teams pwned : 0   │""".format(timediffer(END-int(time())), teamspwned(scrapi))
-    x+="""\n├─ round progress ─────────────────────────────┬─  status codes ────────────────┤"""
-    x+="""\n│       old round: 3 (75.00%)                  │                                │""".format(roundsdone(roundapi,2))
-    x+="""\n│ round began at : 0 (0.00%)                   │                                │""".format(roundbegan(roundapi))
-    x+="""\n├─ stage progress ─────────────────────────────│                                │"""
-    x+="""\n│     game now : team 6                        │                                │"""
-    x+="""\n│   patch rule : 180/384 (46.88%)              │                                │"""
-    x+="""\n│        pcaps : 55.8k                         │                                │"""
-    x+="""\n├───────── total / sla / A / F+ / F- / Status ─┴─────────┬─ brought to you by ──┤"""
-    x+="""\n│    team 1 :    /    /     /     /     /  Code:         │ 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 │""".format()
-    x+="""\n│    team 2 :    /    /     /     /     /  Code:         │ 🔥⚡ blackStab ⚡ 🔥 │"""
-    x+="""\n│    team 3 :    /    /     /     /     /  Code:         │ 🔥          fast  🔥 │"""
-    x+="""\n│    team 4 :    /    /     /     /     /  Code:         │ 🔥           as   🔥 │"""
-    x+="""\n│    team 5 :    /    /     /     /     /  Code:         │ 🔥          f*ck  🔥 │"""
-    x+="""\n│    team 6 :    /    /     /     /     /  Code:         │ 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 │"""
-    x+="""\n│    team 7 :    /    /     /     /     /  Code:         │ 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 │"""
-    x+="""\n│    team 8 :    /    /     /     /     /  Code:         ├──────────────────────┘"""
-    x+="""\n└────────────────────────────────────────────────────────┘  [IST: 21:00]"""#.format(timeinist())
-    print(x)
+    x+="""\n    \x1b[90m\x0e┌─\x1b[36m service milestones \x1b[1;90m────────────────────────────────────┬─ \x1b[36mattack stats \x1b[1;90m──────┐"""
+    x+="""\n    \x1b[90m│\x1b[0m     time in play : \x1b[0;32m{}\x1b[1;90m│    rounds done :\x1b[0;32m{}\x1b[90m│""".format(timediffer(int(time())-START), roundsdone(roundapi, 1))
+    x+="""\n    \x1b[90m│\x1b[0m  from firstblood : \x1b[0;32m{}\x1b[1;90m│flags submitted :\x1b[0;32m{}\x1b[90m│""".format(timefirstblood(), fsubmitted(scrapi))
+    x+="""\n    \x1b[90m│\x1b[0m  firstblood team : \x1b[0;31m{}\x1b[1;90m│   teams pwning :\x1b[31m{}\x1b[90m│""".format(fbloodteam(scrapi), teamspwning(scrapi))
+    x+="""\n    \x1b[90m│\x1b[0m   time remaining : \x1b[0;32m{}\x1b[1;90m│    teams pwned :\x1b[32m{}\x1b[90m│""".format(timediffer(END-int(time())), teamspwned(scrapi))
+    x+="""\n    \x1b[90m├─ \x1b[32mround progress \x1b[1;90m─────────────────────────────┬─ \x1b[32mstatus codes\x1b[90m ─────────────────┤"""
+    x+="""\n    \x1b[90m│       old round: \x1b[0;32m{}\x1b[1;90m│                                │""".format(roundsdone(roundapi,2))
+    x+="""\n    \x1b[90m│ round began at : \x1b[0;32m{}\x1b[1;90m│                                │""".format(roundbegan(roundapi))
+    x+="""\n    \x1b[90m├─ stage progress ─────────────────────────────│                                │"""
+    x+="""\n    \x1b[90m│     game now : \x1b[0;32mno data\x1b[90m                       │                                │"""
+    x+="""\n    \x1b[90m│   patch rule : \x1b[0;32mblackStab policy\x1b[90m              │                                │"""
+    x+="""\n    \x1b[90m│        pcaps : \x1b[0;32mwe're not providing those\x1b[90m     │                                │"""
+    x+="""\n    \x1b[90m├───────── \x1b[32mtotal \x1b[90m/\x1b[32m sla \x1b[90m/ \x1b[32mattack \x1b[90m/ \x1b[32mflg+ \x1b[90m/ \x1b[32mflg-  \x1b[90m┴─────────┬─ brought to you by ──┤"""
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     │                      │""".format(**(scores(scrapi, teamsapi, 0)))
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     │   ⚡ blackStab ⚡    │""".format(**(scores(scrapi, teamsapi, 1)))
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     │                      │""".format(**(scores(scrapi, teamsapi, 2)))
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     │             fast     │""".format(**(scores(scrapi, teamsapi, 3)))
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     │   🔥         as      │""".format(**(scores(scrapi, teamsapi, 4)))
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     │     🔥      f*ck     │""".format(**(scores(scrapi, teamsapi, 5)))
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     │       🔥             │""".format(**(scores(scrapi, teamsapi, 6)))
+    x+="""\n    \x1b[90m│\x1b[0;31m{team}\x1b[90m:{total}/{sla}/{attack}/{fcap}/{flost}/  \x1b[32mcode:\x1b[0m     ├──────────────────────┘""".format(**(scores(scrapi, teamsapi, 7)))
+    x+="""\n    \x1b[90m└────────────────────────────────────────────────────────┘  [IST: 21:00]"""#.format(timeinist())
+    print(x+"\n\n")
 
-def roundsdone(scrapi, when):
-    return eval(scrapi)['round']-when
+def roundsdone(roundapi, when):
+    return str((eval(roundapi)['round']-when)).rjust(4) if when==1 else str((eval(roundapi)['round']-when)).ljust(28)
 
 def timefirstblood():
-    return timediffer(int(time()) - int(open("blood").read()))
+    return timediffer(int(time()) - int(open("blood").read())) if path.exists("blood") else "No one has seen it yet".ljust(37)
 
 def fsubmitted(scrapi):
-    return sum(list(map(lambda x: x['team_score']['scores'][0]['flag_captured'] , (x for x in eval(scrapi)))))
+    return str(sum(list(map(lambda x: x['team_score']['scores'][0]['flag_captured'] , (x for x in eval(scrapi)))))).center(4)
 
 def teamspwning(scrapi):
-    return len(filter((lambda x: x['team_score']['scores'][0]['attack_score'] > 0), (x for x in eval(scrapi))))
+    return str(len(list(filter((lambda x: x['team_score']['scores'][0]['attack_score'] > 0), (x for x in eval(scrapi)))))).center(4)
 
 def teamspwned(scrapi):
-    len(filter((lambda x: x['team_score']['scores'][0]['flag_lost'] > 0), (x for x in eval(scrapi))))
+    return str(len(list(filter((lambda x: x['team_score']['scores'][0]['flag_lost'] > 0), (x for x in eval(scrapi)))))).center(4)
 
 def fbloodteam(bloodapi):
-    return eval(bloodapi)[0]['blood'][0]
+    return eval(bloodapi)[0]['blood'][0].ljust(37) if path.exists("blood") else "No one has seen yet".ljust(37)
 
 def roundbegan(roundapi):
     _530 = (5*60)+(30)
     startat=datetime.strptime((eval(roundapi)['start_time'][:-4]), "%Y-%m-%dT%H:%M:%S.%f")
     startmins = startat.minute + (60)*(startat.hour)
-    return (lambda x: str(x[0]).rjust(2,"0") + ":" + str(x[1]).rjust(2,"0"))(divmod((startmins) + _530, 60))+":"+str(startat.second).rjust(2,"0") + " IST"
+    return ((lambda x: str(x[0]%24).rjust(2,"0") + ":" + str(x[1]).rjust(2,"0"))(divmod((startmins) + _530, 60))+":"+str(startat.second).rjust(2,"0") + " IST").ljust(28)
 
 def timediffer(seconds):
     intervals = (('days', 86400),('hrs', 3600),('min', 60),('sec', 1))
     result = []
     for name, count in intervals:
         value = seconds // count
-        if value:
-            seconds -= value * count
-            if value == 1:
-                name = name.rstrip('s')
-            result.append("{} {}".format(int(value), name))
-    print(result)
-    return ', '.join(result)
+        seconds -= value * count
+        if value == 1:
+            name = name.rstrip('s')
+        result.append("{} {}".format(int(value), name))
+    # print(result)
+    return ', '.join(result).ljust(37)
 
 def scores(scrapi, teamsapi, position):
-    teamname = eval(teamsapi[eval(scrapi)[position]['team_score']['team_id']]['name'])
-    totalscore = '{0:.4g}'.format(eval(scrapi)[position]['team_score']['total_score'])
-    sla = '{0:.4g}'.format(eval(scrapi)[position]['team_score']['sla'])
-    ascore = '{0:.4g}'.format(eval(scrapi)[position]['team_score']['scores'][0]['attack_score'])
+    teamname = eval(teamsapi)[ eval(scrapi)[position]['team_score']['team_id']]['name']
+    totalscore = '{0:.3g}'.format(eval(scrapi)[position]['team_score']['total_score'])
+    sla = str(eval(scrapi)[position]['team_score']['sla'])[:4]
+    ascore = str(eval(scrapi)[position]['team_score']['scores'][0]['attack_score'])[:4]
     fcaptured = str(eval(scrapi)[position]['team_score']['scores'][0]['flag_captured'])
     flost = str(eval(scrapi)[position]['team_score']['scores'][0]['flag_lost'])
 
-    return [teamname, totalscore, totalscore, sla, ascore, fcaptured, flost]
+    return {"team": teamname.rjust(10), "total":totalscore.center(6), "sla": sla.center(6), "attack": ascore.center(6), "fcap": fcaptured.center(5), "flost": flost.center(5)}
+
+def main():
+    while True:
+        print("\033[H\033[2J")
+        template()
+        sleep(0.3)
+
+if __name__ == '__main__':
+    main()
